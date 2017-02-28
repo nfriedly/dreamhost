a125e1987efdce41c0ed7e48536290e72dbc7807
Nathan Friedly <nathan@nfriedly.com>
2017-02-27 22:05:32 -0500

    parent module is complete

---
+++ src/codemods/complete-dreamhost-template.js
@@ -0,0 +1,76 @@
+'use strict';
+const { basename } = require('path');
+const pascalCase = require('pascal-case'); // lol
+const camelCase = require('camel-case');
+const {getModules} = require('../api-commands.js');
+
+
+// takes each file in dist, determines the module from the filename, then renames the internal class and fills in the appropriate methods and docs
+function transformer(file, api) {
+  const j = api.jscodeshift;
+
+  if (basename(file.path) !== 'dreamhost.js') {
+    return;
+  }
+
+  const modules = getModules();
+
+  const ast = j(file.source);
+
+  // replace the base module require with a require statement for each actual module
+  ast.find(j.VariableDeclaration)
+    .filter(p =>
+      // this is probably overly safe, but meh
+      p.value.declarations &&
+      p.value.declarations[0] &&
+      p.value.declarations[0].init &&
+      p.value.declarations[0].init.callee.name === 'require' &&
+      p.value.declarations[0].init.arguments &&
+      p.value.declarations[0].init.arguments[0] &&
+      p.value.declarations[0].init.arguments[0].value === "./base.js"
+    )
+    .replaceWith(() => modules.map(moduleName =>
+        j.variableDeclaration(
+          "const",
+          [j.variableDeclarator(
+            j.identifier(pascalCase(moduleName)),
+            j.callExpression(
+              j.identifier("require"),
+              [j.literal(moduleName)]
+            )
+          )]
+        )
+      )
+    );
+
+
+  // initialize each module in the constructor
+  ast.find(j.MethodDefinition)
+    .filter(p => p.value.kind === 'constructor')
+    .replaceWith(p => {
+      // the block statement AKA the {} that wapps the body of the method
+      // we're going to throw out and replace the body for now.
+      const block = p.value.value.body;
+      // this.moduleName = new ModuleName(options); - for each module
+      block.body = modules.map(moduleName =>
+        j.expressionStatement(
+          j.assignmentExpression(
+            "=",
+            j.memberExpression(
+              j.thisExpression(),
+              j.identifier(camelCase(moduleName))
+            ),
+            j.newExpression(
+              j.identifier(pascalCase(moduleName)),
+              [j.identifier("options")]
+            )
+          )
+        )
+      );
+      return p.value;
+    });
+
+  return ast.toSource();
+}
+
+module.exports = transformer;
--- src/dreamhost-template.js
+++ src/dreamhost-template.js
@@ -5,6 +5,8 @@ const Module = require('./base.js');
 /**
  * Dreamhost JS API
  *
+ * Initializes and provides all submodules with the given key
+ *
  * https://help.dreamhost.com/hc/en-us/sections/203903178-API-Application-Programming-Interface-
  *
  * @constructor
