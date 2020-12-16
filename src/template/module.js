c4808188cf9202c96cc1564fb1f9dbec1aa43fb9
Nathan Friedly <nathan@nfriedly.com>
2020-12-16 15:50:31 -0500

    update to prettier's new prefered code block indentation

--- src/template/module.js
+++ src/template/module.js
@@ -3,13 +3,13 @@
 const BaseDreamHostModule = require('./base.js');
 
 /**
- * ModuleName module
- *
- * @constructor
- * @param {Object} options
- * @param {String} options.key - DreamHost API Key
- * @param {String} [options.api=https://api.dreamhost.com/] DreamHost API URL
- */
+  * ModuleName module
+  *
+  * @constructor
+  * @param {Object} options
+  * @param {String} options.key - DreamHost API Key
+  * @param {String} [options.api=https://api.dreamhost.com/] DreamHost API URL
+  */
 class ModuleName extends BaseDreamHostModule {
   // note: this method isn't actually used by the complete-template.js codemod,
   // but it is representative of the output
